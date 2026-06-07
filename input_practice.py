b1f07f366f271e855691495515a66010659e0af1
Esha Munir <taiwin251600@gmail.com>
2026-06-07 12:54:19 +0500

    Add files via upload

---
+++ input_practice.py
@@ -0,0 +1,11 @@
+
+
+# now learn the use of input. it is used to take input from user in string form 
+a = input("Enter first num: " )
+print(type(a))
+print("first num is: ", a)
+# to convert string into float 
+b = float(input("Enter second num: " ))
+print(type(b))
+print("Second num is: ", b)
+
