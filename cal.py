a0cb82ef6cdcfa8b3eef39e939de56efb517bb84
Esha Munir <taiwin251600@gmail.com>
2026-06-11 23:23:25 +0500

    Add Python calculator
    Added a basic calculator program in Python that performs arithmetic operations such as addition, subtraction, multiplication, and division.

---
+++ cal.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first num:,"))
+b = int(input("Enter second num:, "))
+d = input("Enter the operation, ")
+if d== '+':
+    print(a + b)
+elif d == '-' :
+    print(a - b)
+
+elif d == '*':
+    print(a * b)
+
+elif d == '/' :
+    print(a / b)
